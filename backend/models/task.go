91751e1c1be24c38dd39cb37c16f7135cdb6176a
MelihEmreGuler <melihemreguler@hotmail.com>
2023-12-29 21:51:41 +0300

    change Completed field type to int

--- backend/models/task.go
+++ backend/models/task.go
@@ -6,7 +6,7 @@ type Task struct {
 	ID          string    `bson:"_id,omitempty" json:"id"`
 	Title       string    `bson:"title,omitempty" json:"title"`
 	Description string    `bson:"description,omitempty" json:"description"`
-	Completed   bool      `bson:"completed,omitempty" json:"completed"`
+	Completed   int       `bson:"completed,omitempty" json:"completed"` // 0: not completed, 1: in progress, 2: completed
 	CreateDate  time.Time `bson:"createDate,omitempty" json:"createDate"`
 	DueDate     time.Time `bson:"dueDate,omitempty" json:"dueDate"`
 }
