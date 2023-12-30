d6b08bb2dbe2bb614d68ac644a6762ef95838acb
MelihEmreGuler <melihemreguler@hotmail.com>
2023-12-30 22:34:25 +0300

    add swagger route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	docs "github.com/gdscduzceuniversity/todo-app-1/docs"
 	"github.com/gdscduzceuniversity/todo-app-1/handlers"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func StartServer() error {
@@ -16,18 +19,20 @@ func SetupRouter() *gin.Engine {
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
+	docs.SwaggerInfo.BasePath = "/api"
 	// Simple group: api
 	api := router.Group("/api")
 	{
-		api.GET("/ping", handlers.Ping)
-		//api.POST("/login", loginEndpoint)
 
 		// Auth Routes
-
 		api.POST("/register", handlers.Register)
 		api.POST("/login", handlers.Login)
 		api.POST("/logout", handlers.Logout)
+		//api.GET("/user", handlers.User)
 
 	}
+	// Swagger Route for connect to swagger ui go to http://127.0.0.1:3000/swagger/index.html
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 	return router
 }
