7ac49e1d567b3a383bf943629c7e44b544068c35
MelihEmreGuler <melihemreguler@hotmail.com>
2023-12-31 06:14:03 +0300

    fix compare time info bug

--- backend/middlewares/activity.go
+++ backend/middlewares/activity.go
@@ -1,6 +1,6 @@
 package middlewares
 
-import "github.com/gdscduzceuniversity/todo-app-1/models"
+import "time"
 
 // This middleware will be used for inserting an activity to the database
 // when the completion of the task changes, when the title and description
@@ -9,24 +9,30 @@ import "github.com/gdscduzceuniversity/todo-app-1/models"
 // 0: status change, 1: title, description change, 2: due date change
 
 // CompareTaskByStatus compares two tasks by their completion status.
-func CompareTaskByStatus(oldTask, newTask models.Task) (bool, int) {
-	if oldTask.Completed != newTask.Completed {
+func CompareTaskByStatus(oldCompleted, newCompleted int) (bool, int) {
+	if oldCompleted != newCompleted {
 		return true, 0
 	}
 	return false, -1
 }
 
 // CompareTaskByTitleAndDescription compares two tasks by their title and description.
-func CompareTaskByTitleAndDescription(oldTask, newTask models.Task) (bool, int) {
-	if oldTask.Title != newTask.Title || oldTask.Description != newTask.Description {
+func CompareTaskByTitleAndDescription(oldTitle, oldDescription, newTitle, newDescription string) (bool, int) {
+	if oldTitle != newTitle || oldDescription != newDescription {
 		return true, 1
 	}
 	return false, -1
 }
 
-// CompareTaskByDueDate compares two tasks by their due date.
-func CompareTaskByDueDate(oldTask, newTask models.Task) (bool, int) {
-	if oldTask.DueDate != newTask.DueDate {
+func CompareTaskByDueDate(oldDueDate, newDueDate time.Time) (bool, int) {
+	// The conversion converts both dates to UTC
+	oldDueDateUTC := oldDueDate.UTC()
+	newDueDateUTC := newDueDate.UTC()
+
+	// Comparison in years, months and days
+	if oldDueDateUTC.Year() != newDueDateUTC.Year() ||
+		oldDueDateUTC.Month() != newDueDateUTC.Month() ||
+		oldDueDateUTC.Day() != newDueDateUTC.Day() {
 		return true, 2
 	}
 	return false, -1
